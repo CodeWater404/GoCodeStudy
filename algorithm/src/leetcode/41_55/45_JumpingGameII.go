605d43d11504ce06df560cb4878548682a33da34
codewater404 <1746559397@qq.com>
2023-10-24 23:03:15 +0800

    leetcode: 45

---
+++ algorithm/src/leetcode/41_55/45_JumpingGameII.go
@@ -0,0 +1,18 @@
+package _1_55
+
+/**
+  @author: CodeWater
+  @since: 2023/10/24
+  @desc: 跳跃游戏II
+**/
+func jump(nums []int) int {
+	n := len(nums)
+	f := make([]int, n)
+	for i, j := 1, 0; i < n; i++ {
+		for j+nums[j] < i {
+			j++
+		}
+		f[i] = f[j] + 1
+	}
+	return f[n-1]
+}
