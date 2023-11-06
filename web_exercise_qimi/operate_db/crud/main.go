ecd57c0900be25810666345c388c83aa0bd8eb45
codewater404 <1746559397@qq.com>
2023-11-07 00:41:14 +0800

    web_exercise_qimi: query a data

--- web_exercise_qimi/operate_db/crud/main.go
+++ web_exercise_qimi/operate_db/crud/main.go
@@ -23,7 +23,7 @@ func initDB() (err error) {
 	if ten == "" || tenDb == "" {
 		return errors.New("get env failed, ten:" + ten + ", tenDb:" + tenDb)
 	}
-	dsn = fmt.Sprintf(dsn, ten, tenDb)
+	dsn = fmt.Sprintf(dsn, tenDb, ten)
 
 	//这里不会校验账号和密码
 	db, err = sql.Open("mysql", dsn)
@@ -37,7 +37,33 @@ func initDB() (err error) {
 		return nil
 	}
 
+	//与数据库连接最大数
+	db.SetMaxOpenConns(1)
+	//最大空闲连接
+	db.SetMaxIdleConns(1)
 	return nil
+}
+
+type user struct {
+	id   int64
+	age  int64
+	name string
+}
+
+func queryRowDemo() {
+	sqlStr := "select id , name , age from user where id = ?"
+	var u user
+	//非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放(因为queryrow里面没有释放连接，scan里面释放了)
+	err := db.QueryRow(sqlStr, 1).Scan(&u.id, &u.name, &u.age)
+	/*可以实验：去掉上面的scan
+	再加一行
+	db.QueryRow(sqlStr , 2)
+	然后调用这个函数的时候会发现一直在运行中没有释放连接*/
+	if err != nil {
+		fmt.Printf("scan failed , err: %v\n", err)
+		return
+	}
+	fmt.Printf("====>func queryRowDemo,id:%v , name:%v , age:%v\n", u.id, u.name, u.age)
 }
 
 func main() {
@@ -47,5 +73,6 @@ func main() {
 	}
 	defer db.Close()
 	fmt.Println("db connecnt success!")
-
+	fmt.Println("==========================================================")
+	queryRowDemo()
 }
