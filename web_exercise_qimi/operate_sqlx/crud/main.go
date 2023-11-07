ff9e377fa9fda73a8d0798bf6e4620554501e246
codewater404 <1746559397@qq.com>
2023-11-08 00:03:13 +0800

    web_exercise_qimi: crud with sqlx

--- web_exercise_qimi/operate_sqlx/crud/main.go
+++ web_exercise_qimi/operate_sqlx/crud/main.go
@@ -68,12 +68,123 @@ func queryMultiDemo() {
 	fmt.Printf("users: %#v\n", users)
 
 }
+
+//insertRowDemo 插入一条数据
+func insertRowDemo() {
+	sqlStr := "insert into user (name , age) values(? , ?)"
+	ret, err := db.Exec(sqlStr, "cat", 28)
+	if err != nil {
+		fmt.Printf("insert failed , err:%v\n", err)
+		return
+	}
+	theId, err := ret.LastInsertId()
+	if err != nil {
+		fmt.Printf("get lastInsertId failed , err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success , the id is %d\n", theId)
+}
+
+//updateRowDemo 更新一条数据
+func updateRowDemo() {
+	sqlStr := "update user set age=? where id = ?"
+	ret, err := db.Exec(sqlStr, 66, 6)
+	if err != nil {
+		fmt.Printf("update failed , err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected()
+	if err != nil {
+		fmt.Printf("get rowsAffected failed , err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success , affected rows:%d\n", n)
+}
+
+//deleteRowDemo 删除一条数据
+func deleteRowDemo() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, 6)
+	if err != nil {
+		fmt.Printf("delete failed , err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected()
+	if err != nil {
+		fmt.Printf("get rowsAffected failed , err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success , affected rows:%d\n", n)
+}
+
+//insertUserDemo namedExec绑定sql语句与结构体或map中的同名字段
+func insertUserDemo() (err error) {
+	sqlStr := "insert into user(name , age) values(:name , :age)"
+	_, err = db.NamedExec(sqlStr,
+		map[string]interface{}{
+			"name": "dog",
+			"age":  "32",
+		})
+	return
+}
+
+//namedQuery namedQuery绑定sql语句与结构体或map中的同名字段,不过这里是支持查询的
+func namedQuery() {
+	sqlStr := "select * from user where name=:name"
+	rows, err := db.NamedQuery(sqlStr, map[string]interface{}{
+		"name": "code",
+	})
+	if err != nil {
+		fmt.Printf("namedQuery failed , err:%v\n", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u user
+		err := rows.StructScan(&u)
+		if err != nil {
+			fmt.Printf("scan failed , err:%v\n", err)
+			continue
+		}
+		fmt.Printf("user:%#v\n", u)
+	}
+	u := &user{
+		Name: "qimi",
+	}
+	rows, err = db.NamedQuery(sqlStr, u)
+	if err != nil {
+		fmt.Printf("namedQuery failed , err:%v\n", err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u user
+		err := rows.StructScan(&u)
+		if err != nil {
+			fmt.Printf("scan failed , err:%v\n", err)
+			continue
+		}
+		fmt.Printf("user:%#v\n", u)
+	}
+}
+
 func main() {
 	if err := initDB(); err != nil {
 		fmt.Printf("init db failed , err:%v\n", err)
 		return
 	}
-	fmt.Printf("init db success")
+	fmt.Printf("init db success\n")
 	//queryRowDemo()
-	queryMultiDemo()
+	//queryMultiDemo()
+	//insertRowDemo()
+	//updateRowDemo()
+	//deleteRowDemo()
+
+	//err := insertUserDemo()
+	//if err != nil {
+	//	fmt.Printf("insert failed , err:%v\n", err)
+	//}else {
+	//	fmt.Printf("insert success")
+	//}
+	namedQuery()
 }
