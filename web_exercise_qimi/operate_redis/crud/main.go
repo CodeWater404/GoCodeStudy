6420a0ef33b4cb02f9b4ea6657845a1cc8aab416
codewater404 <1746559397@qq.com>
2023-11-09 00:13:55 +0800

    web_exercise_qimi: operate string and zset for redis

--- web_exercise_qimi/operate_redis/crud/main.go
+++ web_exercise_qimi/operate_redis/crud/main.go
@@ -52,9 +52,88 @@ func initClientCluster() (err error) {
 	return nil
 }
 
+//redisExample set/get案例
+func redisExample() {
+	err := rdb.Set("score", 100, 0).Err()
+	if err != nil {
+		fmt.Printf("set score failed , err:%v\n", err)
+		return
+	}
+	val, err := rdb.Get("score").Result()
+	if err != nil {
+		fmt.Printf("get score failed , err:%v\n", err)
+		return
+	}
+
+	fmt.Printf("score:%v\n", val)
+
+	val2, err := rdb.Get("name").Result()
+	if err == redis.Nil {
+		fmt.Printf("name does not exist..")
+	} else if err != nil {
+		fmt.Printf("get name failed , err:%v\n", err)
+		return
+	} else {
+		fmt.Printf("name:%s\n", val2)
+	}
+}
+
+//redisExample2 zset案列
+func redisExample2() {
+	zsetKey := "lang_rank"
+	languages := []redis.Z{
+		redis.Z{Score: 90.0, Member: "golang"},
+		redis.Z{Score: 98.0, Member: "java"},
+		redis.Z{Score: 95.0, Member: "python"},
+		redis.Z{Score: 97.0, Member: "javaScript"},
+		redis.Z{Score: 99.0, Member: "c/cpp"},
+	}
+	num, err := rdb.ZAdd(zsetKey, languages...).Result()
+	if err != nil {
+		fmt.Printf("zadd failed , err:%v\n", err)
+		return
+	}
+	fmt.Printf("zadd %d succ\n", num)
+
+	newScore, err := rdb.ZIncrBy(zsetKey, 10.0, "golang").Result()
+	if err != nil {
+		fmt.Printf("zincrby failed , err:%v\n", err)
+		return
+	}
+	fmt.Printf("golang's score is %f now \n", newScore)
+
+	//取分数最高的3个
+	ret, err := rdb.ZRevRangeWithScores(zsetKey, 0, 2).Result()
+	if err != nil {
+		fmt.Printf("zrevrange failed , err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Printf("===>%s , %f\n", z.Member, z.Score)
+	}
+
+	//取95-100的
+	op := redis.ZRangeBy{
+		Min: "95",
+		Max: "100",
+	}
+	ret, err = rdb.ZRangeByScoreWithScores(zsetKey, op).Result()
+	if err != nil {
+		fmt.Printf("zrangebyscore failed , err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Printf("===>2 %s , %f\n", z.Member, z.Score)
+	}
+
+}
+
 func main() {
 	if err := initClient(); err != nil {
 		fmt.Printf("init redis failed , err:%v\n", err)
 	}
-	fmt.Printf("init redis success!")
+	fmt.Printf("init redis success!\n")
+
+	//redisExample()
+	redisExample2()
 }
