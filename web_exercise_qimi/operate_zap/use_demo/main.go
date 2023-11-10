c80bc975f9bc02247d51e1e31c978d7a263487dc
codewater404 <1746559397@qq.com>
2023-11-10 17:57:29 +0800

    web_exercise_qimi: use zap in gin

--- web_exercise_qimi/operate_zap/use_demo/main.go
+++ web_exercise_qimi/operate_zap/use_demo/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
-	"net/http"
 )
 
 /**
@@ -87,9 +95,89 @@ func simpleHttpGet(url string) {
 	}
 }
 
+// GinLogger 接收gin框架默认的日志
+func GinLogger(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+
+		cost := time.Since(start)
+		logger.Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost),
+		)
+	}
+}
+
+// GinRecovery recover掉项目可能出现的panic
+func GinRecovery(logger *zap.Logger, stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				// Check for a broken connection, as it is not really a
+				// condition that warrants a panic stack trace.
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					logger.Error(c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+					// If the connection is dead, we can't write a status to it.
+					c.Error(err.(error)) // nolint: errcheck
+					c.Abort()
+					return
+				}
+
+				if stack {
+					logger.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("stack", string(debug.Stack())),
+					)
+				} else {
+					logger.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+				}
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
+	}
+}
+
+//func main() {
+//	InitLogger()
+//	defer logger.Sync()
+//	simpleHttpGet("www.baidu.com")
+//	simpleHttpGet("http://www.baidu.com")
+//}
+
 func main() {
 	InitLogger()
-	defer logger.Sync()
-	simpleHttpGet("www.baidu.com")
-	simpleHttpGet("http://www.baidu.com")
+	//使用自定义gin中间件
+	r := gin.New()
+	r.Use(GinLogger(logger), GinRecovery(logger, true))
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world!")
+	})
+	r.Run()
 }
