f26660ca94104294717df8274fabf8af36e48a92
codewater404 <1746559397@qq.com>
2023-11-11 19:22:04 +0800

    web_exercise_qimi: add some annotation

--- web_exercise_qimi/web_app/settings/settings.go
+++ web_exercise_qimi/web_app/settings/settings.go
@@ -10,12 +10,14 @@ import (
 /**
   @author: CodeWater
   @since: 2023/11/11
-  @desc: $
+  @desc:
+	另外一种读取配置文件的方式： 创建一些结构体，然后使用Unmarshal方法将配置信息直接反序列化到结构体中
 **/
 
 func Init() (err error) {
+	//viper.SetConfigFile("config.json") // 指定配置文件
 	viper.SetConfigName("config")     // 指定配置文件名称（不需要制定配置文件的扩展名）
-	viper.SetConfigType("yaml")       // 指定配置文件类型（专用于从远程获取配置信息时指定配置文件类型）
+	viper.SetConfigType("yaml")       // 指定配置文件类型（专用于从远程etcd获取配置信息时指定配置文件类型）
 	viper.AddConfigPath("./web_app/") // 指定查找配置文件的路径（这里使用相对路径）
 	err = viper.ReadInConfig()        // 读取配置信息
 	if err != nil {
