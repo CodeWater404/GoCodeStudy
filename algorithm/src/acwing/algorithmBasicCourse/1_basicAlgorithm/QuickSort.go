6e21e98e2ca3173941d691baea1b2a48f6ed74ab
codewater404 <1746559397@qq.com>
2023-12-05 01:22:14 +0800

    acwing: 优化 快速排序 读取输入和do-while写法

--- algorithm/src/acwing/algorithmBasicCourse/1_basicAlgorithm/QuickSort.go
+++ algorithm/src/acwing/algorithmBasicCourse/1_basicAlgorithm/QuickSort.go
@@ -79,3 +79,55 @@ func quickSort(q []int, l, r int) {
 	quickSort(q, l, j)
 	quickSort(q, j+1, r)
 }
+
+// 优化读取输入和do-while写法：
+const N2 int = 100010
+
+var (
+	n2      int
+	q2      = make([]int, N2)
+	reader2 = bufio.NewReader(os.Stdin)
+	writer2 = bufio.NewWriter(os.Stdout)
+)
+
+func doScan2(reader *bufio.Reader) []string {
+	str, _ := reader.ReadString('\n')
+	str = strings.TrimSpace(str)
+	return strings.Split(str, " ")
+}
+
+func main2() {
+	data := doScan2(reader2)
+	n2, _ = strconv.Atoi(data[0])
+	data = doScan2(reader2)
+	for i := 0; i < n2; i++ {
+		q2[i], _ = strconv.Atoi(data[i])
+	}
+
+	quick_sort2(q2, 0, n2-1)
+
+	for i := 0; i < n2; i++ {
+		fmt.Fprintf(writer2, "%d ", q2[i])
+	}
+	writer2.Flush()
+}
+
+func quick_sort2(q []int, l, r int) {
+	if l >= r {
+		return
+	}
+	i, j, x := l-1, r+1, q[(l+r)>>1]
+	for i < j {
+		for i++; q[i] < x; i++ {
+		}
+		for j--; q[j] > x; j-- {
+		}
+		if i < j {
+			a := q[i]
+			q[i] = q[j]
+			q[j] = a
+		}
+	}
+	quick_sort2(q, l, j)
+	quick_sort2(q, j+1, r)
+}
