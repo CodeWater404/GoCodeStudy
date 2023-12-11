be56ce359eddee64a5d055e760caaa0f03775ff1
codewater404 <1746559397@qq.com>
2023-12-11 20:51:12 +0800

    acwing: add a new method for high precision subtraction with big library

--- algorithm/src/acwing/algorithmBasicCourse/1_basicAlgorithm/HighPrecisionSubtraction.go
+++ algorithm/src/acwing/algorithmBasicCourse/1_basicAlgorithm/HighPrecisionSubtraction.go
@@ -1,6 +1,9 @@
-package __basicAlgorithm
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 /**
   @author: CodeWater
@@ -8,7 +11,7 @@ import "fmt"
   @desc: 高精度减法
 **/
 
-//比较两个数谁大，大的返回true。
+// 比较两个数谁大，大的返回true。
 func cmp(a, b []int) bool {
 	if len(a) != len(b) {
 		return len(a) > len(b)
@@ -25,17 +28,18 @@ func cmp(a, b []int) bool {
 func sub(a, b []int) []int {
 	var c []int
 	for i, t := 0, 0; i < len(a); i++ {
-		//t表示进位
+		//t表示借位
 		t = a[i] - t
 		if i < len(b) {
 			t -= b[i]
 		}
-		//到这里，a-b就减完了。
+		//到这里，a-b就减完了。因为a-b的结果可能是负数，所以要加10取模；如果是正数，这里其实不影响
 		c = append(c, (t+10)%10)
 		//这里判断a-b的时候是不是用到了上一位，如果t<0说明借位了，把负的变成1，这样下一轮的时候a就会减掉借位的
 		if t < 0 {
 			t = 1
 		} else {
+			// 如果没有借位，就把t变成0，不要影响下一轮的计算。t大于0的情况：说明a[i]本身就比b[i]大，不需要借位
 			t = 0
 		}
 	}
@@ -46,35 +50,48 @@ func sub(a, b []int) []int {
 	return c
 }
 
-func main() {
+// sub2 用math/big包实现高精度减法
+func sub2() {
 	var (
-		a string
-		b string
-		A []int
-		B []int
-		c []int
+		a, b string
+		A, B big.Int
 	)
 	fmt.Scan(&a, &b)
+	A.SetString(a, 10)
+	B.SetString(b, 10)
+	fmt.Println(A.Sub(&A, &B))
+}
 
-	//AB从个位开始读取
-	for i := len(a) - 1; i >= 0; i-- {
-		A = append(A, int(a[i]-'0'))
-	}
-	for i := len(b) - 1; i >= 0; i-- {
-		B = append(B, int(b[i]-'0'))
-	}
+func main() {
+	//var (
+	//	a string
+	//	b string
+	//	A []int
+	//	B []int
+	//	c []int
+	//)
+	//fmt.Scan(&a, &b)
+	//
+	////AB从个位开始读取
+	//for i := len(a) - 1; i >= 0; i-- {
+	//	A = append(A, int(a[i]-'0'))
+	//}
+	//for i := len(b) - 1; i >= 0; i-- {
+	//	B = append(B, int(b[i]-'0'))
+	//}
+	//
+	//if cmp(A, B) {
+	//	c = sub(A, B)
+	//} else {
+	//	//小-大
+	//	c = sub(B, A)
+	//	fmt.Print("-")
+	//}
+	////c也是从个位开始，所以从尾遍历
+	//for i := len(c) - 1; i >= 0; i-- {
+	//	fmt.Print(c[i])
+	//}
+	//fmt.Println()
 
-	if cmp(A, B) {
-		c = sub(A, B)
-	} else {
-		//小-大
-		c = sub(B, A)
-		fmt.Print("-")
-	}
-	//c也是从个位开始，所以从尾遍历
-	for i := len(c) - 1; i >= 0; i-- {
-		fmt.Print(c[i])
-	}
-	fmt.Println()
-
+	sub2()
 }
