2c658092a4f24fa1be5c64abaeca3b5b9b26f360
codewater404 <1746559397@qq.com>
2023-12-13 02:14:29 +0800

    acwing: refactor ThePrefixAnd.go

--- algorithm/src/acwing/algorithmBasicCourse/1_basicAlgorithm/ThePrefixAnd.go
+++ algorithm/src/acwing/algorithmBasicCourse/1_basicAlgorithm/ThePrefixAnd.go
@@ -1,6 +1,12 @@
 package __basicAlgorithm
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 /**
   @author: CodeWater
@@ -8,6 +14,7 @@ import "fmt"
   @desc: 前缀和
 **/
 
+/*
 const N int = 100010
 
 var (
@@ -33,4 +40,42 @@ func main() {
 		fmt.Println(s[r] - s[l-1])
 		m--
 	}
+}
+*/
+
+// ==================== 优化io ====================
+
+const N int = 100010
+
+var (
+	n, m   int
+	a, s   = make([]int, N), make([]int, N)
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+)
+
+func doScan(reader *bufio.Reader) []string {
+	str, _ := reader.ReadString('\n')
+	str = strings.TrimSpace(str)
+	return strings.Split(str, " ")
+}
+
+func main() {
+	data := doScan(reader)
+	n, _ = strconv.Atoi(data[0])
+	m, _ = strconv.Atoi(data[1])
+	data = doScan(reader)
+	for i := 1; i <= n; i++ {
+		a[i], _ = strconv.Atoi(data[i-1])
+		s[i] = s[i-1] + a[i]
+	}
+
+	for m > 0 {
+		data = doScan(reader)
+		l, _ := strconv.Atoi(data[0])
+		r, _ := strconv.Atoi(data[1])
+		fmt.Fprintf(writer, "%d\n", s[r]-s[l-1])
+		m--
+	}
+	defer writer.Flush()
 }
