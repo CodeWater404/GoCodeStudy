581d01473615a1f4195b3e81930dff8be029a3fc
codewater404 <1746559397@qq.com>
2023-12-27 15:59:42 +0800

    leetcode: add notes for 15_ThreeSum.go

--- algorithm/src/leetcode/1_20/15_ThreeSum.go
+++ algorithm/src/leetcode/1_20/15_ThreeSum.go
@@ -12,14 +12,17 @@ func threeSum(nums []int) [][]int {
 	res := [][]int{}
 	sort.Ints(nums)
 	for i := 0; i < len(nums); i++ {
+		//去重，不能包含重复的三元组
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
 		// 双指针
 		for j, k := i+1, len(nums)-1; j < k; j++ {
+			//去重
 			if j > i+1 && nums[j] == nums[j-1] {
 				continue
 			}
+			//试探下一个k是否满足，满足则k--
 			for j < k-1 && nums[i]+nums[j]+nums[k-1] >= 0 {
 				k--
 			}
