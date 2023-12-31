1b29e76448824b104594f1087a21cd25e43bc3e1
codewater404 <1746559397@qq.com>
2023-12-31 21:37:48 +0800

    go-zero: add a demo

---
+++ go-zero/code/helloword/hello/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"go-zero/code/helloword/hello/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
