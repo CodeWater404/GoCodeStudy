50b3e9ab3836e5f1595e01273cbe8bf0ceda1eca
codewater404 <1746559397@qq.com>
2024-01-07 17:24:00 +0800

    go-zero: add api jwt failed callback

--- go-zero/feng/api_study/api_jwt/users.go
+++ go-zero/feng/api_study/api_jwt/users.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"go-zero/feng/common/response"
+	"net/http"
 
 	"go-zero/feng/api_study/api_jwt/internal/config"
 	"go-zero/feng/api_study/api_jwt/internal/handler"
@@ -20,7 +23,7 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(c.RestConf, rest.WithUnauthorizedCallback(JwtUnauthorizedResult))
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
@@ -28,4 +31,10 @@ func main() {
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
+}
+
+// JwtUnauthorizedResult 鉴权失败后的回调处理，详细逻辑还需要再细化，这只是示例
+func JwtUnauthorizedResult(w http.ResponseWriter, r *http.Request, err error) {
+	fmt.Println(err)
+	httpx.WriteJson(w, http.StatusOK, response.Body{10086, "", "鉴权失败"})
 }
