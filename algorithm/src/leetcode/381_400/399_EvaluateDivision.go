5d3a98c6440a257a77d17135e2c7d127b51afe5d
codewater404 <1746559397@qq.com>
2024-01-30 18:30:54 +0800

    leetcode: refactor 399_EvaluateDivision.go

--- algorithm/src/leetcode/381_400/399_EvaluateDivision.go
+++ algorithm/src/leetcode/381_400/399_EvaluateDivision.go
@@ -1,52 +1,54 @@
 package _81_400
 
-/**
-  @author: CodeWater
-  @since: 2023/6/5
-  @desc: 除法求值
-**/
-func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
-	//顶点数组
-	vers := make(map[string]bool)
-	//两点之间的距离
-	d := make(map[string]map[string]float64)
+/*
+*
 
+	@author: CodeWater
+	@since: 2023/6/5
+	@desc: 除法求值
+
+*
+*/
+func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
+	// vers存储所有的点， d[a][b]=val存储a点到b点的权值为val
+	vers, d := make(map[string]bool), make(map[string]map[string]float64)
 	for i := 0; i < len(equations); i++ {
 		a, b := equations[i][0], equations[i][1]
 		c := values[i]
-
-		if _, ok := d[a]; !ok {
+		if _, ok := d[a]; !ok { //首次遍历到a点，分配空间（go特有）
 			d[a] = make(map[string]float64)
 		}
 		if _, ok := d[b]; !ok {
 			d[b] = make(map[string]float64)
 		}
-		//建边上的权值
+		// a到b是c，那么b到a就是1/c
 		d[a][b], d[b][a] = c, 1/c
+		//加入到点集
 		vers[a], vers[b] = true, true
 	}
 
-	//Floyd算法
+	// floyd（类似于无向图）
 	for k := range vers {
 		for i := range vers {
 			for j := range vers {
-				if d[i][k] != 0 && d[k][j] != 0 {
+				_, ok1 := d[i][k]
+				_, ok2 := d[j][k]
+				//存在i到k和j到k，那么i就可以到j
+				if ok1 && ok2 {
 					d[i][j] = d[i][k] * d[k][j]
 				}
 			}
 		}
 	}
 
-	res := make([]float64, len(queries))
-	for i, q := range queries {
+	res := make([]float64, 0)
+	for _, q := range queries {
 		a, b := q[0], q[1]
-		//边上的权值存在，说明存在答案
-		if val, ok := d[a][b]; ok {
-			res[i] = val
+		if _, ok := d[a][b]; ok {
+			res = append(res, d[a][b])
 		} else {
-			res[i] = -1
+			res = append(res, -1)
 		}
 	}
-
 	return res
 }
